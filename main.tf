fd2814643bffbb0936f75efd77aac1b575c4e8bb
viniciushammett <vincius_te@hotmail.com>
2022-09-27 00:47:32 -0300

    Atualizacao Terraform

--- main.tf
+++ main.tf
@@ -24,6 +24,6 @@ resource "aws_instance" "app_server" {
                  nohup busybox httpd -f -p 8080 &
                  EOF
   tags = {
-    Name = "Teste AWS Terraform"
+    Name = "Teste AWS"
   }
 }
